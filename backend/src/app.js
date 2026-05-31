9feb924378e03566c43fe5d6470bad53398b9a41
imran ahamd <imran3111ahamd@gmail.com>
2026-05-31 18:38:48 +0530

    security added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import compression from 'compression';
+import helmet from 'helmet';
 import downloadRoutes from './router/download.router.js';
 
 export const app = express();
 
+app.use(compression());
+app.use(helmet());
 app.use(express.json({limit:'10mb'}));
 app.use(cors({
     origin: [
