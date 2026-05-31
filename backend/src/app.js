3a470cfd4e0d3150db78c0867d3bb604a4082dd5
imran ahamd <imran3111ahamd@gmail.com>
2026-05-31 11:54:48 +0530

    download feature add in video

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,12 @@ import downloadRoutes from './router/download.router.js';
 export const app = express();
 
 app.use(express.json({limit:'10mb'}));
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://socialmediavideodownloader-ashen.vercel.app/",
+        "http://localhost:5173"
+    ]
+}));
 
 app.use('/api',downloadRoutes);
 
