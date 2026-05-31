69ce4c777b5265ac99bc170edbc56469a291a4bd
imran ahamd <imran3111ahamd@gmail.com>
2026-05-31 10:10:24 +0530

    small changes for hosting backend in render

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import { app } from "./src/app.js";
 import 'dotenv/config';
 
+const port = process.env.PORT || 5000;
+
 app.get('/',(req,res)=>{
     return res.status(200).json({
         message:'server is running',
     })
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
     console.log(`server is running on PORT ${process.env.PORT}`)
 })
